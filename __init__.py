8bde9975c385386a2f3020e8b4cfbff09b9022a9
syaofox <syaofox@gmail.com>
2025-05-06 19:30:30 +0800

    变形2

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .inpaint_cropandstitch import InpaintCrop
 from .inpaint_cropandstitch import InpaintStitch
 from .face_morph import FaceMorph
-from .face_shape import FaceShaperMatch
+
 
 WEB_DIRECTORY = "js"
 
@@ -9,14 +9,12 @@ NODE_CLASS_MAPPINGS = {
     "InpaintCrop": InpaintCrop,
     "InpaintStitch": InpaintStitch,
     "FaceMorph": FaceMorph,
-    "FaceShaperMatch": FaceShaperMatch,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "InpaintCrop": "SF Inpaint Crop",
     "InpaintStitch": "SF Inpaint Stitch",
     "FaceMorph": "SF Face Morph",
-    "FaceShaperMatch": "SF Face Shaper Match",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
