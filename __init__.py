689cfebc546d95cbdee6d9ea2b150c7bdaec9f43
syaofox <syaofox@gmail.com>
2025-05-06 22:01:45 +0800

    重构面部分析模块：将OccluderLoader和GeneratePreciseFaceMask类移至新文件face_occluder.py，并更新相关导入路径和映射。

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from .inpaint_cropandstitch import InpaintCrop
 from .inpaint_cropandstitch import InpaintStitch
 from .face_morph import FaceMorph
-from .face_analysis import OccluderLoader
-from .face_analysis import GeneratePreciseFaceMask
+from .face_occluder import OccluderLoader
+from .face_occluder import GeneratePreciseFaceMask
 from .face_analysis import AlignImageByFace
 from .face_analysis import FaceCutout
 from .face_analysis import FacePaste
@@ -14,16 +14,19 @@ from .face_analysis import FaceAnalysisModels
 WEB_DIRECTORY = "js"
 
 
-
-
-
-
 NODE_CLASS_MAPPINGS = {
+    # 局部修复节点
     "InpaintCrop": InpaintCrop,
     "InpaintStitch": InpaintStitch,
+
+    # 人脸变形节点
     "FaceMorph": FaceMorph,
+    
+    # 人脸遮挡节点
     'OccluderLoader': OccluderLoader,
     'GeneratePreciseFaceMask': GeneratePreciseFaceMask,
+
+    # 人脸分析节点
     'AlignImageByFace': AlignImageByFace,
     'FaceCutout': FaceCutout,
     'FacePaste': FacePaste,
@@ -33,12 +36,19 @@ NODE_CLASS_MAPPINGS = {
     
 }
 
-NODE_DISPLAY_NAME_MAPPINGS = {
+NODE_DISPLAY_NAME_MAPPINGS = { 
+    # 局部修复节点
     "InpaintCrop": "SF Inpaint Crop",
     "InpaintStitch": "SF Inpaint Stitch",
+
+    # 人脸变形节点
     "FaceMorph": "SF Face Morph",
+
+    # 人脸遮挡节点
     'OccluderLoader': 'SF Occluder Loader',
     'GeneratePreciseFaceMask': 'SF Generate PreciseFaceMask',
+
+    # 人脸分析节点
     'AlignImageByFace': 'SF Align Image By Face',
     'FaceCutout': 'SF Face Cutout',
     'FacePaste': 'SF Face Paste',
